42c0c53aed7b630854ed4ad1563e0460ee915a89
Antonio Mejorado <amejorado@itesm.mx>
2014-03-05 15:50:03 -0600

    Modifique a collor amarillo para probar esta mugre

--- NetBeansProjects/Project2/src/project2/Project2.java
+++ NetBeansProjects/Project2/src/project2/Project2.java
@@ -14,6 +14,7 @@ public class Project2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int value = 0;
         // TODO code application logic here
     }
 }
